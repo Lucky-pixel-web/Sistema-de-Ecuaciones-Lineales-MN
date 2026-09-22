bbe2b058112627293f2f8305fcec36d9e2fa766c
DIEGO <luckyilose23@gmail.com>
2026-09-21 21:50:56 -0500

    Registrar plugin de tailwind en vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite'   
 
 export default defineConfig({
   base: '/Sistema-de-Ecuaciones-Lineales-MN/',
-  plugins: [react()],
+  plugins: [react()],   
 })
