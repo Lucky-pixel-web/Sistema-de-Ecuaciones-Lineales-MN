e8225a8cae93d4d9a13ff85fe065b01cba224055
DIEGO <luckyilose23@gmail.com>
2026-09-21 21:36:16 -0500

    Proyect Commit 2

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  base: '/Sistema-de-Ecuaciones-Lineales-MN/',
+  plugins: [react()],
 })
